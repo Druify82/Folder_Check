224990b623c0857ff8e612f4822873659a5f30b1
Druify82 <waschmasche@gmail.com>
2025-10-01 21:47:56 +0200

    Letzte Single File Version: Überarbeitung  Kommentare und docstrings
    - Entfernung veralteter Namen von Variablen und Funktionen
    - Vermeidung von Funktions- und Variablennamen (falls sie erneut geändert werden müssen)
    - Aktivere Sprache, kürzere Sätze
    - Unklarheiten als TODO im Code
    - TODO am Schluss des Scripts/Moduls
    - Typos in Text entfernt
    - Variablennamen mit Unterstrichen

--- folder_check.py
+++ folder_check.py
@@ -3,19 +3,23 @@
 # Die Shebang-Zeile ist die erste Zeile eines Scripts. Sie legt am
 # Anfang des Scripts unabhängig vom Betriebssystem fest, dass dieses Skript
 # mit Python 3 ausgeführt werden soll.
-# Erforderlich für Linux, empfohlen für Windows und andere Betriebssysteme.
+# Erforderlich wenn unter Unix als ausführbare Datei markiert.
+# Nicht erforderlich für Windows und andere Betriebssysteme.
 
 # Kommentare und DocStrings
 #
-# Ein DocString, ausgeschrieben Documentation String =
+# Ein docstring, ausgeschrieben Documentation String =
 # Dokumentations-Zeichenfolge ist mehrzeiliger Text in jeweils
-# dreifachem Anführungszeichen (""") in der ersten und letzten Zeile. Im
-# DocString beschreibt man am Dokumentenanfang das Programm/Modul. Unter der
-# Funktionsdefinition "def" beschreibt man, was die Funktion tut. Diese Texte
-# kann man für eine automatisierte Dokumentation verwenden.
-# Anders als Kommentare mit Nummernzeichen (#) ist der DocString nur am
-# Dokumentanfang und unter einer neuen Funktion erlaubt. Er wird später zur
-# Dokumentation benutzt.
+# dreifachem Anführungszeichen (""") vor und nach dem Dokumentationstext.
+# Weniger üblich sind alternativ dreifach einfache Anführungszeichen ('''). Der
+# Text darf auf einer Zeile stehen oder mehrzeilig sein. Mehrzeiliger Text
+# braucht kein eigenes Kommentarzeichen am Zeilenanfang.
+# Am Anfang des Moduls (der Datei) beschreibt man das Programm oder Modul: Nach
+# Shebang und Angabe der Codierung, aber vor allen anderen Anweisungen.
+# Unter der Definition von Funktionen und Klassen "def" beschreibt man, was die
+# Funktion oder Klasse tut.
+# Diese Texte kann man für eine automatisierte Dokumentation verwenden.
+# An anderen Stellen hat der DocString keine Bedeutung.
 #
 # Kommentare beginnen am Zeilenanfang oder nach einer Codezeile mit
 # Nummernzeichen (#). Abgesehen von der Shebang-Zeile benutzt man sie nur, um
@@ -27,7 +31,7 @@
 
 """
 foldercheck.py – Einsteiger-Skript für FolderCheck
-Version: 0.2.0pre, 2025-08-20, 156:04
+Version: 0.2.0-pre, 2025-09-03, 14:10
 
 Kurzbeschreibung: Phase 2: Ausgabe-Modularisierung, Einführung Git
 Autor: Druify, <waschmasche@gmail.com>
@@ -39,7 +43,7 @@ Beschreibung: Ordner überprüfen nach verschiedensten Kriterien
 
 - Funktionen:
   - Zählt alle Dateien rekursiv.
-  - Zählt alle Ordner rekursiv unter.
+  - Zählt alle Ordner und Unterordner rekursiv.
   - Statistikausgabe
   - CLI
   - Main-Funktion
@@ -61,8 +65,11 @@ import argparse
 # Typen beschreiben
 # Wenn man dies nur in der Funktion festlegt, ist die Wörterbuchstruktur nur
 # in dieser Funktion verständlich, wird aber sonst nicht zuverlässig erkannt.
-# Innerhalb der Liste muss man die Einträge nicht mit Komma (,), sondern mit
-# senkrechtem Strich (|) voneinander trennen.
+# Wenn man dies nicht festlegt, kann der Linter die Struktur
+# unerwartet falsch interpretieren. Dies ist also eine Absicherung.
+# Innerhalb der Annotation trennt der Union Operator,
+# ein senkrechter Strich (|) die Typen voneinander. In Listen trennt man die
+# Elemente weiterhin mit Komma (,).
 StatsType = dict[str, list[str | int | bool]]
 
 # Zunächst definiert man die verschiedenen Funktionen. Erst danach erstellt
@@ -73,42 +80,45 @@ StatsType = dict[str, list[str | int | bool]]
 # später abrufen.
 
 
-def countfiles(countfiles_path: str) -> int:
+def count_files(count_files_path: str) -> int:
     """
     Zählt rekursiv alle Dateien im Verzeichnis einschl. Unterordner.
-    :param countfiles_path, str: Eingabe Pfad (String) zum Ordner
-    :return -> int: Ausgabe Anzahl Integer/Ganzzahl) der gefundenen Dateien
+    :param count_files_path, str: Eingabe Pfad (String) zum Ordner
+    :return -> int: Ausgabe Anzahl Integer/Ganzzahl der gefundenen Dateien
     """
-    # Variablen müssen zunächst initialisiert werden mit einem Anfangswert.
-    countfiles_value = 0
+    # Man muss die Variable nicht wie in anderen Programmiersprachen
+    # initialisieren mit dem Inhaltstypen. Aber man muss einmalig einen
+    # Anfangswert angeben.
+    count_files_value = 0
     # os muss als übergeordnetes Modul mit der Methode walk benannt werden.
     # os.walk gibt ein Tupel zurück: (root, dirs, files)
     # Wir benutzen die Variablen root und dirs aus dem Tupel nicht. Daher
     # schreiben wir einen Unterstrich (_) davor oder lassen den Namen ganz weg.
-    for _, _, files in os.walk(countfiles_path):
+    for _, _, files in os.walk(count_files_path):
         # 0 + len(files) gibt Anzahl der Dateien in der Liste files zurück
-        countfiles_value += len(files)
-    return countfiles_value
+        count_files_value += len(files)
+    return count_files_value
 
 
-def countdirs(countdirs_path: str) -> int:
+def count_dirs(count_dirs_path: str) -> int:
     """
     Zählt rekursiv alle Ordner im Verzeichnis einschl. Unterordner.
     """
-    countdirs_value = 0
-    for _, dirs, _ in os.walk(countdirs_path):
-        countdirs_value += len(dirs)
-    return countdirs_value
+    count_dirs_value = 0
+    for _, dirs, _ in os.walk(count_dirs_path):
+        count_dirs_value += len(dirs)
+    return count_dirs_value
 
 
 # Befehlszeile
 
 
 def parse_cmdline() -> argparse.Namespace:
-    # Die obige Definition besagt: Die Klasse nimmt keine Eingaben. Die Ausgabe
-    # ist ein Namespace-Objekt. Darin sind die eingegebenen Argumente
-    # enthalten, die der Benutzer eingegeben hat. Deshalb wird später nicht die
-    # Funktion gestartet, sondern eine Variable befüllt.
+    # Diese Funktion hat keine Parameter. Sie nimmt keine Eingaben an. Der
+    # Rückgabewert, die Ausgabe, ist ein Namespace-Objekt. Darin sind die
+    # eingegebenen Argumente enthalten, die der Benutzer eingegeben hat.
+    # Deshalb führt man später nicht die Funktion aus, sondern man befüllt
+    # damit eine Variable.
     """
     CLI (Command Line Interface / Befehlszeilen-Oberfläche) mit argparse
     Diese Funktion erstellt mit dem importierten Modul argparse einen
@@ -119,7 +129,8 @@ def parse_cmdline() -> argparse.Namespace:
     Kommandozeilenargumente enthält.
     Diese Funktion wird aufgerufen, um die Kommandozeilenargumente zu parsen
     und zurückzugeben. So kann die Main-Funktion sie verwenden.
-    Der Benutzer ruft auf: programme.py <Befehlszeilenoptionen>
+    Der Benutzer ruft auf:
+    <PythonScript>.py <Befehlszeilenoptionen>
     In dieser Parser-Funktion steht, welche Möglichkeiten zulässig sind, welche
     Hilfetexte angezeigt werden.
     Diese Funktion legt nicht fest, was mit den zulässigen Eingaben passiert.
@@ -127,7 +138,7 @@ def parse_cmdline() -> argparse.Namespace:
     # Erstellen des ArgumentParsers
     # parser enthält alles, was zulässig ist auf der Befehlszeile.
     parser = argparse.ArgumentParser(
-        # Beschreibung des Skripts
+        # Diese Beschreibung wird auf der Kommandozeile angezeigt.
         # TODO: Aktuell unklar, wie man den Text mit f-Strings oder anderweitig
         # formatiert angeben kann.
         description='FolderCheck Phase 2: Statistiken für Ordner und '
@@ -143,10 +154,11 @@ def parse_cmdline() -> argparse.Namespace:
         # Eingabetyp Text/String
         type=str,
         # Standardwert: Statt Punkt für aktuellen Arbeitsordner ist os.getcwd()
-        # möglich (cwd = current working directory)
+        # möglich
+        # cwd = current working directory
         default=os.getcwd(),
-        # nargs: Numer of arguments legt fest, wie viele Eingaben zum Argument
-        # gehören
+        # nargs: number of arguments legt fest, wie viele Eingaben zum Argument
+        # gehören.
         nargs='?',
         help="Pfad zum zu analysierenden Ordner. Standard: aktueller Ordner"
     )
@@ -157,14 +169,10 @@ def parse_cmdline() -> argparse.Namespace:
         # dest= benötigt man, wenn man den Inhalt unter einem anderen Namen
         # abrufen möchte.
         dest="dirs",
-        # action="store_true": standardmäßig ist das Flag gesetzt
-        # TODO: Prüfen, ob richtig verstanden!
-        # Wenn Flag angegeben wird, args.dirs auf True, andernfalls setze
-        # es auf False.
-        # Dies ist nützlich, um zu entscheiden, ob nur Ordner oder auch Dateien
-        # gezählt werden sollen.
-        # Wenn das Flag gesetzt ist, wird die Anzahl der Ordner gezählt.
-        # Wenn das Flag nicht gesetzt ist, wird dies nicht gezählt.
+        # action="store_true": Setzt das Flag auf Boolean True, wenn es auf
+        # der Befehlszeile vorkommt. Es bleibt sonst False.
+        # Dies ist nützlich, um abzufragen, ob der Benutzer das Flag Argument
+        # auf der Befehlszeile angegeben hat.
         action="store_true",
         help="Ordner zählen"
     )
@@ -173,26 +181,29 @@ def parse_cmdline() -> argparse.Namespace:
         "--files", "-f",
         dest="files",
         action="store_true",
-        help="Dateien  zählen"
+        help="Dateien zählen"
     )
     # Eingaben parsen (auswerten)
-    # Die Eingaben werden nun auf zwei Arten ausgewertet:
-    # 1. Lange Variante: Ausgabe des Parsers in einer temporären Variable:
+    # Die Eingaben kann man nun auf zwei Arten auswerten:
+    # 1. Lange Variante: Ausgabe des Parsers in einer temporären Variable
+    # Notwendig, wenn man das Ergebnis vor der Übergabe weiter auswerten
+    # möchte.
     # args = parser.parse_args()
     # return args
-    # TODO: Warum muss args nicht initialisiert werden wie andere
-    # Variblen/Objekte?
-    # Hier wird args ein Namespace-Objekt, das die eingegebenen Argumente
-    # enthält. parse_args() wertet das Objekt parser aus.
+    # Die Variable args wird hier befüllt. Man kann sie danach sofort
+    # verwenden. Hier wird args ein Namespace-Objekt, das die eingegebenen
+    # Argumente als Attribute enthält. parse_args() wertet das Objekt parser
+    # aus.
     # Wenn die Eingaben gültig sind, werden  sie in args gespeichert.
     # Wenn die Eingaben ungültig sind, erscheint eine Fehlermeldung und
     # das Programm wird beendet.
-    # args = parser.args() allein würde die Argumente parsen und in der
-    # Variable args speichern. Erst return machtdie Argumente zur Ausgabe
+    # args = parser.parse_args() allein würde die Argumente parsen und in der
+    # Variable args speichern. Erst return macht die Argumente zur Ausgabe
     # dieser Funktion. Dann ist sie für andere Funktionen zugänglich.
     # 2. Kurze Variante: Ausgabe des Parsers direkt
+    # Geeignet, wenn hier nichts weiter passiert, z.B. eine Auswertung in
+    # einer anderen Funktion oder einem anderen Modul erfolgt.
     # return parser.parse_args()
-    # TODO: Wann ist die kurze Variante besser, wann die lange?
     return parser.parse_args()
 
 
@@ -215,22 +226,22 @@ def print_stats() -> None:
     # Dictionary (Wörterbuch) für Zählungen
     # Das Dictionary ruft die Funktionen auf, die die Ergebnisse für die
     # Statistik liefern.
-    # Die Schlüssel sind die Namen der Statistiken, die Werte sind Tupel mit
-    # dem Anzeigetext,  der ausgeführten Funktion sowie einem bool'schen Wert,
-    # um die spätere Anzeige zu (de)aktivieren.
-    # Die Werte sind Listen, die den Status der Statistik enthalten. Sonst
-    # wären sie immutables (Unveränderliche).
-    # Standard wäre: stats = {, hier aber um Beschreibung erweitert.
-    # Alias legt fest: Dies ist ein Dictionary mit den Typen:
-    # str,→ Liste aus str | int | bool
+    # Die Schlüssel sind die Namen der Statistiken. Die Werte sind eine Liste
+    # mit dem Anzeigetext, der ausgeführten Funktion sowie einem boolschen
+    # Wert, um die spätere Anzeige zu (de)aktivieren.
+    # Die Liste ermöglicht, dass die Einträge veränderlich sind.
+    # Standard für die Ankündigung eines Wörterbuchs wäre:
+    # stats = {
+    # Doch hier wird die Ankündigung um die global typannotierte Beschreibung
+    # erweitert. Damit ist die Struktur für den Linter eindeutig.
     stats: StatsType = {
-        "stats_files": ["Anzahl Dateien", countfiles(args.path), False],
-        "stats_dirs": ["Anzahl Ordner", countdirs(args.path), False]
+        "stats_files": ["Anzahl Dateien", count_files(args.path), False],
+        "stats_dirs": ["Anzahl Ordner", count_dirs(args.path), False]
     }
     # Ausgabe der Ergebnisse
     # Die Ausgabe erfolgt in der Konsole.
     # Teil 1: Statistikausgabe vorbereiten, gewünschte Einträge auf True
-    # TODO: Die if-Schleife soll wie folgt flexibler werden
+    # TODO: Die if-Bedingung soll wie folgt flexibler werden
     # Frage ohne die genauen namen zu wissen ab, welche Befehlszeilenargumente
     # angegeben wurden und stelle sie auf true. Wichtig, wenn immer mehr
     # Einstellungen dazu kommen.
@@ -252,10 +263,10 @@ def print_stats() -> None:
     # Effizienter: Diese Variante ruft nur auf, was benötigt wird.
     for stats_text, stats_value, stats_on in stats.values():
         if stats_on:  # Wenn der Eintrag auf True gesetzt ist
-            print(f"{stats_text} in {args.path}: {stats_value}")
+            print(f'{stats_text} in "{args.path}": {stats_value}')
             # Stelle dies auf true, weil etwas gedruckt wurde.
             printed = True
-    # Achtung: Dieser if-Teil gehört nicht mehr zur for-SChleife!
+    # Achtung: Dieser if-Teil gehört nicht mehr zur for-Schleife!
     # Die For-Schleife sagt also nur: Gebe etwas aus, oder tue nichts.
     if not printed:
         print(f"Nichts gezählt in {args.path}")
@@ -285,8 +296,9 @@ def main() -> None:
 # Hauptfunktion nur aufrufen, wenn das Skript direkt ausgeführt wird
 # Die Verwendung dieser Zeile ist eine Good Practice, damit Teile des Scripts
 # auch einzeln aufgerufen werden dürfen.
-# Wenn dies fehlt, wird jeder Funktionsaufruf von außen ganze Skript von
-# main() aus ausführen, statt nur der jeweiligen Funktion.
+# Mit dieser Code-Zeile startet main() nur beim direkten Ausführen, nicht beim
+# Importieren des Moduls.
+# Damit ist es möglich, einzelne Funktionen von außen aufzurufen.
 if __name__ == "__main__":
     main()
 
