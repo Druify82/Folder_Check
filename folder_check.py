8ccf2863e3e85c2b22cd1dcee709bfb24694c6a4
Druify82 <waschmasche@gmail.com>
2025-10-08 20:38:56 +0200

    Phase2: Modularisierung der Erzeugung von Statistiken in fc_stats

--- folder_check.py
+++ folder_check.py
@@ -56,11 +56,11 @@ Struktur:
 
 
 # Diese Module werden benötigt.
-# Funktionen für Statistikerstellung aus Betriebssystem (os = Operating System)
-import os
 
 # Befehlszeile auswerten
 import fc_cli
+# Statistiken erstellen
+import fc_stats
 
 # printstats: Global das Wörterbuch mit verschachtelter Liste und enthaltenen
 # Typen beschreiben
@@ -75,40 +75,7 @@ StatsType = dict[str, list[str | int | bool]]
 
 # Zunächst definiert man die verschiedenen Funktionen. Erst danach erstellt
 # man eine Funktion, die tatsächlich etwas ausführt.
-
-
-# Informationen ermitteln: Andere Funktionen werden die folgenden Informationen
-# später abrufen.
-
-
-def count_files(count_files_path: str) -> int:
-    """
-    Zählt rekursiv alle Dateien im Verzeichnis einschl. Unterordner.
-    :param count_files_path, str: Eingabe Pfad (String) zum Ordner
-    :return -> int: Ausgabe Anzahl Integer/Ganzzahl der gefundenen Dateien
-    """
-    # Man muss die Variable nicht wie in anderen Programmiersprachen
-    # initialisieren mit dem Inhaltstypen. Aber man muss einmalig einen
-    # Anfangswert angeben.
-    count_files_value = 0
-    # os muss als übergeordnetes Modul mit der Methode walk benannt werden.
-    # os.walk gibt ein Tupel zurück: (root, dirs, files)
-    # Wir benutzen die Variablen root und dirs aus dem Tupel nicht. Daher
-    # schreiben wir einen Unterstrich (_) davor oder lassen den Namen ganz weg.
-    for _, _, files in os.walk(count_files_path):
-        # 0 + len(files) gibt Anzahl der Dateien in der Liste files zurück
-        count_files_value += len(files)
-    return count_files_value
-
-
-def count_dirs(count_dirs_path: str) -> int:
-    """
-    Zählt rekursiv alle Ordner im Verzeichnis einschl. Unterordner.
-    """
-    count_dirs_value = 0
-    for _, dirs, _ in os.walk(count_dirs_path):
-        count_dirs_value += len(dirs)
-    return count_dirs_value
+# Hier sind diese Funktionen ausgelagert in eigenen Modulen.
 
 
 # Ausgabe der Statistik
@@ -139,9 +106,11 @@ def print_stats() -> None:
     # Doch hier wird die Ankündigung um die global typannotierte Beschreibung
     # erweitert. Damit ist die Struktur für den Linter eindeutig.
     stats: StatsType = {
-        "stats_files": ["Anzahl Dateien", count_files(args.path), False],
-        "stats_dirs": ["Anzahl Ordner", count_dirs(args.path), False]
-    }
+        "stats_files": [
+            "Anzahl Dateien", fc_stats.count_files(
+                args.path), False], "stats_dirs": [
+            "Anzahl Ordner", fc_stats.count_dirs(
+                args.path), False]}
     # Ausgabe der Ergebnisse
     # Die Ausgabe erfolgt in der Konsole.
     # Teil 1: Statistikausgabe vorbereiten, gewünschte Einträge auf True
