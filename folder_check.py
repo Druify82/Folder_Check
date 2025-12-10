0d4718145f1671f4072f3e894a27cff2ec757e39
Druify <waschmasche@gmail.com>
2025-12-10 19:24:47 +0100

    Init Phase3 Load Path:

--- folder_check.py
+++ folder_check.py
@@ -31,7 +31,7 @@
 
 """
 foldercheck.py – Einsteiger-Skript für FolderCheck
-Version: 0.2.0, 2025-11-26
+Version: 0.3.0pre, 2025-11-27
 
 Kurzbeschreibung: Phase 2: Erste modularisierte Version
 Autor: Druify, <waschmasche@gmail.com>
